83dafced908e188000bdee92bb5ed7bc968d07eb
phalusphilip <phalusphilip@outlook.com>
2023-02-16 13:10:54 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 /**
- * main - A program that print lins with puts functions
- * return: 0
- * (Done)
- */
+* main - Entry point
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
