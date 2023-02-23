dbc4c20a735301b8fa90091d3152f40ea5f878a6
phalusphilip <phalusphilip@outlook.com>
2023-02-23 03:11:01 -0800

    is upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,10 +4,9 @@
  * Main : check for upper case characters
  * Return : return 1 if c is uppercase, otherwise 0.
  */
-
 int _isupper(int c)
 {
-	if (c = 'A' && c = 'Z')
+	if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
 		return (0);
