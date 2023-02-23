ef633715e6275dae11a02995c2e5b038f16528ff
phalusphilip <phalusphilip@outlook.com>
2023-02-23 02:23:40 -0800

    is upper case

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * Main : check for upper case characters
+ * Return : return 1 if c is uppercase, otherwise 0.
+ */
+
+int _isupper(int c)
+{
+	if (c = 'A' && c = 'Z')
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,9 @@
+/**
+ * This is the header for main.h 
+ */
+#ifndef MAIN_H
+#define MAIN_H
+
+int _isupper(int c);
+
+#endif
